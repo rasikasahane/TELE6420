67b12639f03c7e1bca468475c79a530a7078f749
rasikasahane <itssahane.rasika@gmail.com>
2024-11-02 16:26:05 -0400

    Changing code to have current date with time to developer1

--- show_time.py
+++ show_time.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
 
-current_time = datetime.now().strftime("%H:%M:%S")
+current_datetime = datetime.now()
+current_date = current_datetime.strftime("%Y-%m-%d")
+current_time = current_datetime.strftime("%H:%M:%S")
 
-
+print("Current Date:", current_date)
 print("Current Time:", current_time)
