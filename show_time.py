49a93def7508a3c848acc9420c0d5dae5bcf8ed4
rasikasahane <itssahane.rasika@gmail.com>
2024-11-02 16:29:32 -0400

    Adding code for Day in developer 2

--- show_time.py
+++ show_time.py
@@ -4,6 +4,8 @@ from datetime import datetime
 current_datetime = datetime.now()
 current_date = current_datetime.strftime("%Y-%m-%d")
 current_time = current_datetime.strftime("%H:%M:%S")
+current_day = current_datetime.strftime("%A")
 
 print("Current Date:", current_date)
 print("Current Time:", current_time)
+print("Day:", current_day)
